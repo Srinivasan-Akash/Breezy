0eee6968eedf376b9d24d9de8af9b04f744f8a31
Srinivasan-Akash <prashanthiakash@gmail.com>
2023-03-30 15:24:40 +0530

    Added timeout for request and even allow user to send or recieve data which an be converted to ArrayBuffer, Blob or formData

--- Browser API's/Fetch API/DELETE.js
+++ Browser API's/Fetch API/DELETE.js
@@ -1,50 +1,95 @@
-export default function DELETE(url, contentType) {
+export default function DELETE(url, contentType, data, timeout = 60000) {
   const controller = new AbortController();
   const signal = controller.signal;
 
-  if (contentType == "json") {
-    return {
-      promise: fetch(url, {
-        method: "DELETE",
-        headers: { "Content-Type": "application/json" },
-        signal,
-      }).then((response) => {
-        return response.json().then((json) => {
-          return {
-            json_response: json,
-            status: response.status,
-            response_type: response.type,
-            response_header: response.headers,
-            response_body: response.body,
-            response_body_used: response.bodyUsed,
-          };
-        });
-      }),
-      cancel: () => controller.abort(),
-    };
-  } else if (contentType == "xml") {
-    return {
-      promise: fetch(url, {
-        method: "DELETE",
-        headers: { "Content-Type": "application/xml" },
-        signal,
-      }).then((response) => {
-        return response.text().then((xml) => {
-          const parser = new DOMParser();
-          const xmlDoc = parser.parseFromString(xml, "text/xml");
-          const serializer = new XMLSerializer();
-          const xmlStr = serializer.serializeToString(xmlDoc);
-          return {
-            xml_response: xmlStr,
-            status: response.status,
-            response_type: response.type,
-            response_header: response.headers,
-            response_body: response.body,
-            response_body_used: response.bodyUsed,
-          };
-        });
-      }),
-      cancel: () => controller.abort(),
-    };
-  }
+  const promise = new Promise((resolve, reject) => {
+    let headers = {};
+
+    let body = data;
+    if (contentType == "json") {
+      headers = { "Content-Type": "application/json" };
+      body = JSON.stringify(data);
+    } else if (contentType == "xml") {
+      headers = { "Content-Type": "application/xml" };
+    } else if (contentType == "form-data") {
+      headers = {};
+      body = new FormData();
+      for (let key in data) {
+        body.append(key, data[key]);
+      }
+    } else if (contentType == "array-buffer") {
+      headers = {};
+      body = new ArrayBuffer(data.length);
+      let uint8View = new Uint8Array(body);
+      for (let i = 0; i < data.length; i++) {
+        uint8View[i] = data[i];
+      }
+    } else if (contentType == "blob") {
+      headers = {};
+      body = new Blob([data], { type: "application/octet-stream" });
+    }
+    const timeoutId = setTimeout(() => {
+      controller.abort();
+      reject("Breeze:- Request timed out");
+    }, timeout);
+
+    fetch(url, {
+      method: "DELETE",
+      headers,
+      body,
+      signal,
+    })
+      .then((response) => {
+        clearTimeout(timeoutId);
+        if (response.ok) {
+          if (contentType == "json") {
+            return response.json().then((json) => {
+              resolve({
+                json_response: json,
+                status: response.status,
+                response_type: response.type,
+                response_header: response.headers,
+                response_body: response.body,
+                response_body_used: response.bodyUsed,
+              });
+            });
+          } else if (contentType == "xml") {
+            return response.text().then((xml) => {
+              const parser = new DOMParser();
+              const xmlDoc = parser.parseFromString(xml, "text/xml");
+              const serializer = new XMLSerializer();
+              const xmlStr = serializer.serializeToString(xmlDoc);
+              resolve({
+                xml_response: xmlStr,
+                status: response.status,
+                response_type: response.type,
+                response_header: response.headers,
+                response_body: response.body,
+                response_body_used: response.bodyUsed,
+              });
+            });
+          } else {
+            resolve({
+              status: response.status,
+              response_type: response.type,
+              response_header: response.headers,
+              response_body: response.body,
+              response_body_used: response.bodyUsed,
+            });
+          }
+        } else {
+          reject(response.status + " " + response.statusText);
+        }
+      })
+      .catch((error) => {
+        clearTimeout(timeoutId);
+        if (error.name === "AbortError") {
+          reject("Request was cancelled");
+        } else {
+          reject(error);
+        }
+      });
+  });
+
+  return { promise, cancel: () => controller.abort() };
 }
--- Browser API's/Fetch API/PATCH.js
+++ Browser API's/Fetch API/PATCH.js
@@ -1,65 +1,94 @@
-export default function PATCH(url, contentType, data) {
+export default function PATCH(url, contentType, data, timeout = 60000) {
   const controller = new AbortController();
   const signal = controller.signal;
 
   const promise = new Promise((resolve, reject) => {
+    let headers = {};
+
+    let body = data;
     if (contentType == "json") {
-      fetch(url, {
-        method: "PATCH",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(data),
-        signal
-      })
-        .then((response) => {
-          return response.json().then((json) => {
-            resolve({
-              json_response: json,
-              status: response.status,
-              response_type: response.type,
-              response_header: response.headers,
-              response_body: response.body,
-              response_body_used: response.bodyUsed
-            });
-          });
-        })
-        .catch((error) => {
-          if (error.name === "AbortError") {
-            reject("Request was cancelled");
-          } else {
-            reject(error);
-          }
-        });
+      headers = { "Content-Type": "application/json" };
+      body = JSON.stringify(data);
     } else if (contentType == "xml") {
-      fetch(url, {
-        method: "PATCH",
-        headers: { "Content-Type": "application/xml" },
-        body: data,
-        signal
-      })
-        .then((response) => {
-          return response.text().then((xml) => {
-            const parser = new DOMParser();
-            const xmlDoc = parser.parseFromString(xml, "text/xml");
-            const serializer = new XMLSerializer();
-            const xmlStr = serializer.serializeToString(xmlDoc);
+      headers = { "Content-Type": "application/xml" };
+    } else if (contentType == "form-data") {
+      headers = {};
+      body = new FormData();
+      for (let key in data) {
+        body.append(key, data[key]);
+      }
+    } else if (contentType == "array-buffer") {
+      headers = {};
+      body = new ArrayBuffer(data.length);
+      let uint8View = new Uint8Array(body);
+      for (let i = 0; i < data.length; i++) {
+        uint8View[i] = data[i];
+      }
+    } else if (contentType == "blob") {
+      headers = {};
+      body = new Blob([data], { type: "application/octet-stream" });
+    }
+    const timeoutId = setTimeout(() => {
+      controller.abort();
+      reject("Breeze:- Request timed out");
+    }, timeout);
+
+    fetch(url, {
+      method: "PATCH",
+      headers,
+      body,
+      signal,
+    })
+      .then((response) => {
+        clearTimeout(timeoutId);
+        if (response.ok) {
+          if (contentType == "json") {
+            return response.json().then((json) => {
+              resolve({
+                json_response: json,
+                status: response.status,
+                response_type: response.type,
+                response_header: response.headers,
+                response_body: response.body,
+                response_body_used: response.bodyUsed,
+              });
+            });
+          } else if (contentType == "xml") {
+            return response.text().then((xml) => {
+              const parser = new DOMParser();
+              const xmlDoc = parser.parseFromString(xml, "text/xml");
+              const serializer = new XMLSerializer();
+              const xmlStr = serializer.serializeToString(xmlDoc);
+              resolve({
+                xml_response: xmlStr,
+                status: response.status,
+                response_type: response.type,
+                response_header: response.headers,
+                response_body: response.body,
+                response_body_used: response.bodyUsed,
+              });
+            });
+          } else {
             resolve({
-              xml_response: xmlStr,
               status: response.status,
               response_type: response.type,
               response_header: response.headers,
               response_body: response.body,
-              response_body_used: response.bodyUsed
+              response_body_used: response.bodyUsed,
             });
-          });
-        })
-        .catch((error) => {
-          if (error.name === "AbortError") {
-            reject("Request was cancelled");
-          } else {
-            reject(error);
           }
-        });
-    }
+        } else {
+          reject(response.status + " " + response.statusText);
+        }
+      })
+      .catch((error) => {
+        clearTimeout(timeoutId);
+        if (error.name === "AbortError") {
+          reject("Request was cancelled");
+        } else {
+          reject(error);
+        }
+      });
   });
 
   return { promise, cancel: () => controller.abort() };
